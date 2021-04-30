b66f99e8adda08be376d87b30307b1ab93c8ebb1
choibyunghyeon <choi8608@gmail.com>
2021-04-30 23:36:41 +0900

    feat: 자동차 구현

---
+++ src/main/java/racingcar/car/Car.java
@@ -0,0 +1,72 @@
+package racingcar.car;
+
+import java.util.Objects;
+import java.util.Random;
+
+import racingcar.config.Property;
+
+public class Car implements Comparable<Car> {
+
+	private final Name name;
+	private final Engine engine;
+	private final Distance distance;
+
+	public Car(String name) {
+		this.name = new Name(name);
+		this.engine = new Engine();
+		this.distance = new Distance();
+	}
+
+	public Car(Car newCar) {
+		this.name = new Name(newCar.getName());
+		this.engine = new Engine();
+		this.distance = new Distance(newCar.distance);
+	}
+
+	public void drive() {
+		int randomInput = new Random().nextInt(Property.MAX_RANDOM_NUMBER + 1);
+		distance.add(engine.drive(randomInput));
+	}
+
+	public void drive(Integer input) {
+		distance.add(engine.drive(input));
+	}
+
+	public String getName() {
+		return name.getName();
+	}
+
+	public Integer getTotalDistance() {
+		return distance.sum();
+	}
+
+	/**
+	 * 이름이 같다면 같은 자동차로 간주됩니다.
+	 * @param o
+	 * @return
+	 */
+	@Override
+	public boolean equals(Object o) {
+		if (this == o)
+			return true;
+		if (!(o instanceof Car))
+			return false;
+		Car car = (Car)o;
+		return getName().equals(car.getName());
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(getName());
+	}
+
+	/**
+	 * 총 주행 거리를 비교합니다.
+	 * @param compareCar
+	 * @return
+	 */
+	@Override
+	public int compareTo(Car compareCar) {
+		return compareCar.getTotalDistance().compareTo(this.getTotalDistance());
+	}
+}
